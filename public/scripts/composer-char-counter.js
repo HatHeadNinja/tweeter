6807b6209a56815aa4a30ff0aab536f74a4f7ba3
Doug Dyson <dougdyson@gmail.com>
2020-09-12 06:15:31 +0000

    unsuccessful attempt to set className dynamically

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,15 +8,19 @@ $(document).ready(function() {
     
     if (charCount === 0){
       $('.counter').val('0 / 140');
+      // trying to change the class instead of style, can't make it work!
+      // $('.counter').className = 'new-tweet div button';
+      $('.counter').css("color", "darkgray");
       $('#new-tweet-button').prop("disabled", true);
       $('#new-tweet-button').css("background-color", "grey");
       $('#tweet-label').css("display", "flex");
       $('#error').css("display","none");
-      $('.counter').css("color", "darkgray");
     
     } else if (charCount > 0 && charCount < 141) {
       alerted = false;
       $('.counter').val(charCount + ' / 140');
+      // trying to change the class instead of style but can't make it work!
+      // $('.counter').className = 'counter';
       $('.counter').css("color", "darkgray");
       $('#new-tweet-button').prop("disabled", false);
       $('#new-tweet-button').css("background-color", "#4056A1");
@@ -27,6 +31,8 @@ $(document).ready(function() {
       alerted = true;
       $('.counter').val(charCount + ' / 140');
       $('.counter').css("color", "crimson");
+      // trying to change the class instead of style but can't make it work!
+      // $('.counter').className = 'error';
       $('#new-tweet-button').prop("disabled", true);
       $('#new-tweet-button').css("background-color", "grey");
       $('#tweet-label').css("display", "none");
@@ -34,6 +40,8 @@ $(document).ready(function() {
     } else if (charCount > 141) {
       charCount = 140 - charCount;
       $('.counter').val(charCount + ' / 140');
+      // trying to change the class instead of style but can't make it work!
+      // $('.counter').className = 'error';
       $('.counter').css("color", "crimson");
       $('#new-tweet-button').prop("disabled", true);
       $('#new-tweet-button').css("background", "grey");
