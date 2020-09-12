78513925dfed85e061915060ba7cb3cb46ebb154
Doug Dyson <dougdyson@gmail.com>
2020-09-12 02:33:05 +0000

    removed used code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   // DEAR EVALUATOR PERSON
   // ======================
 
-  // Processes all targetted user events with length from the tweet input
+  // Processes tweet character length passed from the tweet input listeners
   const charCounter = function (charCount) {
     
     if (charCount === 0){
@@ -16,12 +16,12 @@ $(document).ready(function() {
       $('#tweet-label').css("display", "flex");
       $('#error').css("display","none");
       $('.counter').css("color", "darkgray");
-      //return;
+    
     } else if (charCount > 0 && charCount < 141) {
       alerted = false;
       $('.counter').val(charCount + ' / 140');
-      $('#new-tweet-button').prop("disabled", false);
       $('.counter').css("color", "darkgray");
+      $('#new-tweet-button').prop("disabled", false);
       $('#new-tweet-button').css("background-color", "#4056A1");
       $('#tweet-label').css("display", "flex");
       $('#error').css("display","none");
@@ -29,25 +29,24 @@ $(document).ready(function() {
     } else if (charCount === 141 && alerted === false) {
       alerted = true;
       $('.counter').val(charCount + ' / 140');
-      $('#new-tweet-button').prop("disabled", true);
       $('.counter').css("color", "crimson");
-      $('#tweet-label').css("display", "none");
+      $('#new-tweet-button').prop("disabled", true);
       $('#new-tweet-button').css("background-color", "grey");
-      errorMsg('Too many characters! Say more with less...');
+      $('#tweet-label').css("display", "none");
 
     } else if (charCount > 141) {
       charCount = 140 - charCount;
-      $('#new-tweet-button').prop("disabled", true);
-      $('#new-tweet-button').css("background", "grey");
       $('.counter').val(charCount + ' / 140');
       $('.counter').css("color", "crimson");
+      $('#new-tweet-button').prop("disabled", true);
+      $('#new-tweet-button').css("background", "grey");
       $('#tweet-label').css("display", "none");
       $('tweet-text').css("border-bottom", "1px solid red");
       $('#error').css("display","flex");
     }
   }
   
-  // Capture events in the tweet input and sends tweet length to charCounter function
+  // Capture events in the tweet input and send tweet length to charCounter function
   $('#tweet-text').on('keydown', function() {
     const charCount = $(this).val().length;
     charCounter(charCount);
@@ -67,14 +66,5 @@ $(document).ready(function() {
     const charCount = $(this).val().length;
     charCounter(charCount);
   });
-  
-  function errorMsg(errMsg) {
-    if ($("#error").first().is(":hidden")) {
-      $("#error").val(errMsg);
-      $('#error').css("display","flex");
-    } else {
-      $("#error").hide();
-    }
-  }
 
 });
